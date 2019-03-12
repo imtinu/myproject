9a0e29bfde685197135c82432f7e86e3e08a8fa0
narayan yadav <ydvnarayan@gmail.com>
2019-03-12 11:52:14 +0530

    narayan add new testclass

---
+++ src/com/hopscotch/tests/newdemo.java
@@ -0,0 +1,12 @@
+package com.hopscotch.tests;
+
+import org.testng.Reporter;
+import org.testng.annotations.Test;
+
+public class newdemo {
+@Test
+public void newtest()
+{
+Reporter.log("hello");	
+}
+}
